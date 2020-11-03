af9d72c0047ccea135d743c81de9176bd36e7107
Kristian <kristian.risvaer@gmail.com>
2020-11-03 11:55:36 +0100

    Oppdatert

--- lib.js
+++ lib.js
@@ -1,14 +1,14 @@
 
 // Hovedfunksjon
 function isDateValid(dato) {
-    let day = dato.slice(0,2);
-    let month = dato.slice(3,5);
-    let year = dato.slice(6,10);
+    // let day = dato.slice(0,2);
+    // let month = dato.slice(3,5);
+    // let year = dato.slice(6,10);
 
     return (isLength(dato)&&
         thirdAndSixth(dato)&&
-        isYear(year)&&
-        isMonth(month)&&
+        isYear(dato)&&
+        isMonth(dato)&&
         isDay(dato));
 }
 
@@ -22,26 +22,28 @@ function thirdAndSixth(dato) {
 }
 
 // Funksjon som sjekker om år er 0000 eller større
-function isYear(year) {
+function isYear(dato) {
+    let year = dato.slice(6,10);
     return (year.length === 4 && year >= '0000' && year <= '9999')
 }
 
 // Funksjon som sjekker om måned er fra og med 01 til og med 12
-function isMonth(month) {
+function isMonth(dato) {
+    let month = dato.slice(3,5);
     return month >= '01' && month <= '12' ? true : false;
 }
 
 // Funksjon som sjekker om dagen er gyldig
-function isDay(day, month, year) {
-    return isDayfrom1To28(day, month, year) === true || (isLeapYear(year) && month === '02' && day === '29') === true || is31(day, month) === true || is30NotFeb(day, month) === true ? true : false;
+function isDay(dato) {
+    let day = dato.slice(0,2);
+    let month = dato.slice(3,5);
+    let year = dato.slice(6,10);
+    return isDayfrom1To28(day) || (isLeapYear(year) && month === '02' && (day >= '01' && day <= '29')) || is31(day, month) || is30NotFeb(dato);
 }
 
 // Funksjon som sjekker om dag er fra og med 01 til og med 28
-function isDayfrom1To28(day, month, year) {
-    return (day >= '01' && day <= '28' || 
-    day === '29' && month === '02' && 
-    isLeapYear(year) === true ||
-    day === '30' && month != '02')
+function isDayfrom1To28(day) {
+    return (day >= '01' && day <= '28')
 }
 // Funksjon som sjekker om det er skuddår
 function isLeapYear(year)
@@ -50,15 +52,17 @@ function isLeapYear(year)
 }
 // Funksjon som sjekker om dag 31 er gyldig
 function is31(day, month) {
-    return (day == '31' && month == '01' ||
-        day == '31' && month == '03' ||
-        day == '31' && month == '05' ||
-        day == '31' && month == '07' ||
-        day == '31' && month == '08' ||
-        day == '31' && month == '10' ||
-        day == '31' && month == '12')
+    return (day === '31' && month === '01' ||
+        day === '31' && month === '03' ||
+        day ==='31' && month === '05' ||
+        day === '31' && month === '07' ||
+        day === '31' && month === '08' ||
+        day === '31' && month === '10' ||
+        day === '31' && month === '12')
 }
 // Funksjon som sjekker om dag er 30 og måned ikke er februar
-function is30NotFeb(day, month) {
-    return day == '30' && month != '02' ? true : false;
+function is30NotFeb(dato) {
+    let day = dato.slice(0,2);
+    let month = dato.slice(3,5);
+    return (day >= '01' && day <= '30') && month != '02';
 }
